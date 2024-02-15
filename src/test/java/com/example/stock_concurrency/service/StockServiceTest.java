9d467e78074c453868efd5e11efe9cf6d344bdaf
Hongseob Lee <sjun2918@naver.com>
2024-02-15 20:55:06 +0900

    update: 동시에 100번의 재고 감소 요청 테스트 - 레이스 컨디션이 일어난다.

--- src/test/java/com/example/stock_concurrency/service/StockServiceTest.java
+++ src/test/java/com/example/stock_concurrency/service/StockServiceTest.java
@@ -2,6 +2,10 @@ package com.example.stock_concurrency.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,5 +47,31 @@ class StockServiceTest {
 
 		//then
 		assertThat(stock.getQuantity()).isEqualTo(99);
+	}
+
+	@Test
+	@DisplayName("동시에 100개 요청")
+	void 동시에_100개_요청() throws Exception {
+
+		//given
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		//when
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		//then
+		assertThat(stock.getQuantity()).isEqualTo(0);
 	}
 }
